6b2cf43768ea753a1f54516225aeb20753f49c0d
hfael <fael.henine@gmail.com>
2025-12-18 15:09:23 +0100

    update(global): Ajout de donnée dans les tables avec fichiers json

--- utils.go
+++ utils.go
@@ -17,6 +17,7 @@ type Handler func([]string) error
 var handlers = map[string]Handler{
 	"create database": handlerCreateDatabase,
 	"create table":    handlerCreateTable,
+	"insert into":     handlerInsertInto,
 }
 
 var (
@@ -54,6 +55,80 @@ func handlerCreateTable(parts []string) error {
 	if err != nil {
 	}
 	return err
+}
+func handlerInsertInto(parts []string) error {
+
+	if len(parts) < 6 {
+		return ErrInvalidSyntax
+	}
+
+	table := strings.Trim(parts[2], `"`)
+	database := strings.Trim(parts[4], `"`)
+
+	values := make(map[string]any)
+
+	for _, p := range parts[6:] {
+		kv := strings.SplitN(p, "=", 2)
+		if len(kv) != 2 {
+			return ErrInvalidSyntax
+		}
+
+		key := strings.Trim(kv[0], `"`)
+		val := strings.Trim(kv[1], `"`)
+
+		values[key] = val
+	}
+
+	return insertInto(database, table, values)
+}
+
+func insertInto(database, table string, values map[string]any) error {
+	tablePath := filepath.Join(
+		strings.ToUpper(database),
+		strings.ToUpper(table),
+	)
+
+	schemaPath := filepath.Join(tablePath, "schema.json")
+
+	schemaBytes, err := os.ReadFile(schemaPath)
+	if err != nil {
+		return err
+	}
+
+	var schema struct {
+		Columns map[string]string `json:"columns"`
+	}
+	if err := json.Unmarshal(schemaBytes, &schema); err != nil {
+		return err
+	}
+
+	primaryKey := "name"
+
+	for col := range schema.Columns {
+		if _, ok := values[col]; !ok {
+			return ErrIncompletSyntax
+		}
+	}
+
+	pkValue, ok := values[primaryKey]
+	if !ok {
+		return ErrIncompletSyntax
+	}
+
+	filename := strings.ReplaceAll(
+		strings.TrimSpace(pkValue.(string)),
+		" ",
+		"_",
+	) + ".json"
+
+	rowPath := filepath.Join(tablePath, filename)
+
+	out, err := json.MarshalIndent(values, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(rowPath, out, 0644)
 }
 
 func parseColumn(line string) error {
